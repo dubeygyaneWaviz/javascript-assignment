a5e66acb835b405d9eba2fc7df9a30d7b1e86695
Maneesh Kumar <maneeshsri@gmail.com>
2023-06-27 10:16:53 +0530

    Added console logs

--- ashwi/easy/addNo.js
+++ ashwi/easy/addNo.js
@@ -1,13 +1,11 @@
 function addNumber(num){
     let sum =0;
-
     for (let i=1; i<=num; i++){
-       //sum+=i; We can use this statement also
-       sum=sum+i;
-       console.log("for current value of i " +i+ " sum is ", sum);
-
+        console.log("for current value of i " +i+ " sum is ", sum);
+        //sum+=i; We can use this statement also
+        sum=sum+i;
+        console.log("for current value of i " +i+ " sum is ", sum);
     }
     return sum;
-
 }
 console.log(addNumber(4));
